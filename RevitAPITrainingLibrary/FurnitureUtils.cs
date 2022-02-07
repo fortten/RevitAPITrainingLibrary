1c037bd7b7becb312f70f0389a7e61e5d66fdf3d
sergey leonov <redfortten@hotmail.com>
2022-02-07 16:21:49 +0300

    FamilyInstanceUtils, FamilySymbolUtils, FurnitureUtils added

---
+++ RevitAPITrainingLibrary/FurnitureUtils.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class  FurnitureUtils
+    {
+        public static List<FamilySymbol> GetFurniture(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilySymbol> furniture = new FilteredElementCollector(doc)
+                                                .OfClass(typeof(FamilySymbol))
+                                                .OfCategory(BuiltInCategory.OST_Furniture)
+                                                .Cast<FamilySymbol>()
+                                                .ToList();
+            return furniture;
+        }
+    }
+}
